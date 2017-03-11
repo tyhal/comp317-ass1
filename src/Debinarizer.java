aa4ed66ec8a5d80d9cb4beaf8860dae4c9247d5a
xavier.simmons <xavier.simmons@nyriad.com>
2017-03-11 14:32:48 +1300

    Binarize I think comlete

--- src/Debinarizer.java
+++ src/Debinarizer.java
@@ -3,16 +3,24 @@ import java.io.*;
 
 public class Debinarizer {
     private static BufferedWriter bw;
+    private long phrase_count = 0;
+    public final long reset_phrase_num = 0; // Ensured to work with any dictionary limit
 
+    public void debinarize( int bit ) {
+    	
+    }
+    
     public static void main(String[] args) throws IOException {
 
-        BufferedReader br = new BufferedReader(new FileReader(args[1]));
-        bw = new BufferedWriter(new FileWriter(args[0]));
+    	BufferedReader br = new BufferedReader( new InputStreamReader( System.in, "UTF-8" ) );
+    	bw = new BufferedWriter( new OutputStreamWriter( System.out, "UTF-8" ) );
+
+    	Debinarizer debinarizer = new Debinarizer();
 
         try {
-            int i = 0, c;
-            while( ( c = br.read() ) != -1 ) {
-                bw.write( c );
+            int bit;
+            while( ( bit = br.read() ) != -1 ) {
+            	debinarizer.debinarize( bit );
             }
         } catch( IOException e ) {
             e.printStackTrace();

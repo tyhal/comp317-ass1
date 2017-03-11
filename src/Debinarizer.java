b72f1ae0c54978ab985d00fe930651f67dfcb513
xavier.simmons <xavier.simmons@nyriad.com>
2017-03-11 19:40:32 +1300

    trying..

--- src/Debinarizer.java
+++ src/Debinarizer.java
@@ -3,11 +3,63 @@ import java.io.*;
 
 public class Debinarizer {
     private static BufferedWriter bw;
+    
     private long phrase_count = 0;
+    private long bit_string_count = 0;
+    private long phrase_num_length = 0;
+    
+    private long binary_string = 0;
+//    private int consecutive_byte_debinarize_count = 1;
+    private boolean debinarize_byte = true;
+    
     public final long reset_phrase_num = 0; // Ensured to work with any dictionary limit
-
-    public void debinarize( int bit ) {
+    
+    private String format( char c ) {
+		return Integer.toHexString( (int)c );
+	}
+    
+    public void debinarize( int bit, boolean last ) throws IOException {
     	
+    	binary_string ^= bit;
+    	bit_string_count++;
+    	
+//    	System.out.println( bit + " " + phrase_count + " " + Long.toBinaryString(binary_string));
+    	
+//    	if( consecutive_byte_debinarize_count > 0 ) {
+    	if( debinarize_byte ) {
+    		if( bit_string_count == 8 ) {
+    			if( /*consecutive_byte_debinarize_count == 1 &&*/ phrase_count == 0 ) {
+	    			bw.write( Long.toString( 1 ) );
+	    		}
+    		
+	    		bw.write( "," + format( (char)binary_string ) + "\n" ); // Write mismatch byte
+	    		
+//	    		if( --consecutive_byte_debinarize_count == 0 )
+	    		{
+	    			phrase_count++;
+	    			phrase_num_length = Long.SIZE - Long.numberOfLeadingZeros( phrase_count );
+	    		}
+	    		
+	    		bit_string_count = 0;
+	    		binary_string = 0;
+	    		debinarize_byte = false;
+    		}
+    	} else if ( bit_string_count == phrase_num_length ) {
+    		bw.write( Long.toString( binary_string ) + ( last ? "\n" : "" ) );
+    		debinarize_byte = true;
+//    		if( binary_string == reset_phrase_num ) {
+//    			phrase_count = 0;
+//    			consecutive_byte_debinarize_count = 2;
+//    		} else {
+//    			consecutive_byte_debinarize_count = 1;
+//    		}
+
+    		bit_string_count = 0;
+    		binary_string = 0;
+    		phrase_num_length = 0;
+    	}
+    	
+    	binary_string <<= 1; 
     }
     
     public static void main(String[] args) throws IOException {
@@ -18,10 +70,12 @@ public class Debinarizer {
     	Debinarizer debinarizer = new Debinarizer();
 
         try {
-            int bit;
-            while( ( bit = br.read() ) != -1 ) {
-            	debinarizer.debinarize( bit );
-            }
+        	int next_c, c = br.read();
+    		for( boolean last = ( c == -1 ); !last; c = next_c ) {
+    			last = ( ( next_c = br.read() ) == -1 );
+    			debinarizer.debinarize( Character.getNumericValue( (char)c ), last );
+    		}
+
         } catch( IOException e ) {
             e.printStackTrace();
         } finally {
@@ -31,4 +85,3 @@ public class Debinarizer {
 
     }
 }
-

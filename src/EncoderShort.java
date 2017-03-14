4c12158545e5ad34013c5683c17731beed3c3e90
xavier.simmons <xavier.simmons@nyriad.com>
2017-03-14 18:56:40 +1300

    EncoderShort now supports reset

--- src/EncoderShort.java
+++ src/EncoderShort.java
@@ -2,25 +2,31 @@ import java.util.*;
 import java.io.*;
 public class EncoderShort {
     private class Trie {
-        public long phrase_num = phrase_count++; // Sets phrase_num and increments phrase count on construction
+        public long phrase_num = ++phrase_count; // Sets phrase_num and increments phrase count on construction
         public TreeMap< Character, Trie > trie = new TreeMap< Character, Trie >();
     };
-    private long phrase_count = 1;
-    private Trie dict = new Trie(), dict_ptr = dict;
+    private long phrase_count = 0; // 0 is RESET phrase number
+    private Trie dict = new Trie(), dict_curr = dict;
     public static void main( String[] args ) throws IOException {
     	BufferedReader br = new BufferedReader( new InputStreamReader( System.in, "UTF-8" ) );
         BufferedWriter bw = new BufferedWriter( new OutputStreamWriter( System.out, "UTF-8" ) );
-        EncoderShort enc = new EncoderShort();
+        int dict_limit = 1 << Integer.parseInt( args[ 0 ] ); // 2 ^ max_num_bits
+        EncoderShort enc = new EncoderShort(); // Create encoder with empty dictionary
     	for( int next_c, c = br.read(); c != -1; c = next_c ) {
-			boolean last = ( ( next_c = br.read() ) == -1 ), in_dict = enc.dict_ptr.trie.containsKey( (char)c );
-			if( in_dict ) {
-				enc.dict_ptr = enc.dict_ptr.trie.get( (char)c );
+			boolean last = ( ( next_c = br.read() ) == -1 ), in_dict = enc.dict_curr.trie.containsKey( (char)c );
+			if( enc.phrase_count == dict_limit )  { // Dictionary full
+				bw.write( Long.toString( ( enc.phrase_count = 0 ) /* RESET phrase # */ ) + "," + Integer.toHexString( c ) + "\n" );
+				enc = new EncoderShort(); // Reset dictionary
+			} else {
+				if( in_dict ) {
+					enc.dict_curr = enc.dict_curr.trie.get( (char)c ); // Traverse Trie
+				}
+	            if( !in_dict || last ) {
+	                enc.dict_curr.trie.put( (char)c, enc.new Trie() ); // Add to dictionary
+	                bw.write( Long.toString( enc.dict_curr.phrase_num ) + ((last && in_dict) ? "\n" : "," + Integer.toHexString( c ) + "\n" ) );
+	                enc.dict_curr = enc.dict; // Reset head back to start of Trie
+	            }
 			}
-            if( !in_dict || last ) {
-                enc.dict_ptr.trie.put( (char)c, enc.new Trie() ); // Add to dictionary
-                bw.write( Long.toString( enc.dict_ptr.phrase_num ) + ((last && in_dict) ? "\n" : "," + Integer.toHexString( c ) + "\n" ) );
-                enc.dict_ptr = enc.dict; // Reset head back to start
-            }
         }
 		br.close();
 		bw.close();

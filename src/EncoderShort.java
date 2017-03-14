3c6598038d1c173029032adac0918e37faed9311
xavier.simmons <xavier.simmons@nyriad.com>
2017-03-14 22:43:12 +1300

    Added DecoderShort - working

--- src/EncoderShort.java
+++ src/EncoderShort.java
@@ -14,26 +14,26 @@ public class EncoderShort {
     private Trie dict = new Trie(), dict_curr = dict;
     public static void main( String[] args ) throws IOException {
         final BufferedReader br = new BufferedReader( new InputStreamReader( System.in, "UTF-8" ) );
-        final BufferedWriter bw = new BufferedWriter( new OutputStreamWriter( System.out, "UTF-8" ) );
         final int dict_limit = 1 << Integer.parseInt( args[ 0 ] ); // 2 ^ max_num_bits
         EncoderShort encoder = new EncoderShort(); // Create encoder with empty dictionary
         for( int next_c, c = br.read(); c != -1; c = next_c ) {
-            final boolean last = ( ( next_c = br.read() ) == -1 ), in_dict = encoder.dict_curr.trie.containsKey( (char)c );
+            final boolean last = ( ( next_c = br.read() ) == -1 );
             if( encoder.phrase_count == dict_limit )  { // Dictionary full
-                bw.write( Long.toString( ( encoder.phrase_count = 0 ) /* RESET phrase # */ ) + "," + Integer.toHexString( c ) + "\n" );
+                System.out.println( ( encoder.phrase_count = 0 /* RESET phrase # */ ) + "," + Integer.toHexString( c ) );
 	        encoder = new EncoderShort(); // Reset dictionary
             } else {
-                if( in_dict ) {
+                if( encoder.dict_curr.trie.containsKey( (char)c ) ) { // Phrase is dictionary
                     encoder.dict_curr = encoder.dict_curr.trie.get( (char)c ); // Traverse Trie
-                }
-                if( !in_dict || last ) {
+                    if( last ) {
+                        System.out.println( encoder.dict_curr.phrase_num );
+                    }
+                } else {
                     encoder.dict_curr.trie.put( (char)c, encoder.new Trie() ); // Add to dictionary
-                    bw.write( Long.toString( encoder.dict_curr.phrase_num ) + ((last && in_dict) ? "\n" : "," + Integer.toHexString( c ) + "\n" ) );
+                    System.out.println( encoder.dict_curr.phrase_num + "," + Integer.toHexString( c ) );
                     encoder.dict_curr = encoder.dict; // Reset head back to start of Trie
                 }
             }
         }
         br.close();
-        bw.close();
     }
 }

966cf65654bdbcbaa31ba93c9d645dc2ebe97249
xavier.simmons <xavier.simmons@nyriad.com>
2017-03-14 22:43:12 +1300

    Added DecoderShort - working

---
+++ src/DecoderShort.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.AbstractMap;
+import java.util.ArrayList;
+import java.util.Map;
+
+public class DecoderShort {
+    public static BufferedWriter bw;
+    private ArrayList< Map.Entry< Long, Character > > dict = new ArrayList< Map.Entry< Long, Character > >();
+    public void decode( long dict_idx, char prefix, boolean has_mismatch ) throws IOException {
+        if( dict_idx >= 0 ) { // Phrase exists in dictionary
+            Map.Entry< Long, Character > pair = dict.get( ( int )dict_idx );
+            decode( pair.getKey() - 2, pair.getValue(), true ); // Traverse to root 1st then build up phrase from there
+        }
+        if( has_mismatch ) {
+            bw.write( prefix );
+	}
+    }
+    public static void main( String[] args ) throws IOException {
+        final BufferedReader br = new BufferedReader( new InputStreamReader( System.in, "UTF-8" ) );
+        bw = new BufferedWriter( new OutputStreamWriter( System.out, "UTF-8" ) );
+    	DecoderShort decoder = new DecoderShort();
+        for( String line = br.readLine(); line != null; line = br.readLine() ) {
+            String[] tokens = line.split( ",", 2 ); // Limit to 2 for case where value is ','
+            long phrase_num = Long.parseLong( tokens[ 0 ] );
+            char prefix = tokens.length == 2 ? (char)Integer.parseInt( tokens[ 1 ], 16 ) : '\0' /*can be anything*/;
+            if( tokens.length == 2 && phrase_num != 0 /* RESET phrase number */ ) { // False when last pair doesn't have mismatch
+                decoder.dict.add( new AbstractMap.SimpleEntry< Long, Character >( phrase_num, prefix ) );
+            } else if( phrase_num == 0 ) {
+                decoder.dict.clear();
+            }
+            decoder.decode( phrase_num - 2, prefix, tokens.length == 2 );
+        }
+        br.close();
+        bw.close();
+    }
+}
--- src/EncoderShort.java
+++ src/EncoderShort.java
@@ -13,27 +13,27 @@ public class EncoderShort {
     private long phrase_count = 0; // 0 is RESET phrase number
     private Trie dict = new Trie(), dict_curr = dict;
     public static void main( String[] args ) throws IOException {
-    	final BufferedReader br = new BufferedReader( new InputStreamReader( System.in, "UTF-8" ) );
+        final BufferedReader br = new BufferedReader( new InputStreamReader( System.in, "UTF-8" ) );
         final BufferedWriter bw = new BufferedWriter( new OutputStreamWriter( System.out, "UTF-8" ) );
         final int dict_limit = 1 << Integer.parseInt( args[ 0 ] ); // 2 ^ max_num_bits
-        EncoderShort enc = new EncoderShort(); // Create encoder with empty dictionary
-    	for( int next_c, c = br.read(); c != -1; c = next_c ) {
-			final boolean last = ( ( next_c = br.read() ) == -1 ), in_dict = enc.dict_curr.trie.containsKey( (char)c );
-			if( enc.phrase_count == dict_limit )  { // Dictionary full
-				bw.write( Long.toString( ( enc.phrase_count = 0 ) /* RESET phrase # */ ) + "," + Integer.toHexString( c ) + "\n" );
-				enc = new EncoderShort(); // Reset dictionary
-			} else {
-				if( in_dict ) {
-					enc.dict_curr = enc.dict_curr.trie.get( (char)c ); // Traverse Trie
-				}
-	            if( !in_dict || last ) {
-	                enc.dict_curr.trie.put( (char)c, enc.new Trie() ); // Add to dictionary
-	                bw.write( Long.toString( enc.dict_curr.phrase_num ) + ((last && in_dict) ? "\n" : "," + Integer.toHexString( c ) + "\n" ) );
-	                enc.dict_curr = enc.dict; // Reset head back to start of Trie
-	            }
-			}
+        EncoderShort encoder = new EncoderShort(); // Create encoder with empty dictionary
+        for( int next_c, c = br.read(); c != -1; c = next_c ) {
+            final boolean last = ( ( next_c = br.read() ) == -1 ), in_dict = encoder.dict_curr.trie.containsKey( (char)c );
+            if( encoder.phrase_count == dict_limit )  { // Dictionary full
+                bw.write( Long.toString( ( encoder.phrase_count = 0 ) /* RESET phrase # */ ) + "," + Integer.toHexString( c ) + "\n" );
+	        encoder = new EncoderShort(); // Reset dictionary
+            } else {
+                if( in_dict ) {
+                    encoder.dict_curr = encoder.dict_curr.trie.get( (char)c ); // Traverse Trie
+                }
+                if( !in_dict || last ) {
+                    encoder.dict_curr.trie.put( (char)c, encoder.new Trie() ); // Add to dictionary
+                    bw.write( Long.toString( encoder.dict_curr.phrase_num ) + ((last && in_dict) ? "\n" : "," + Integer.toHexString( c ) + "\n" ) );
+                    encoder.dict_curr = encoder.dict; // Reset head back to start of Trie
+                }
+            }
         }
-		br.close();
-		bw.close();
+        br.close();
+        bw.close();
     }
 }

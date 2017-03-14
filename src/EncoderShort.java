0938966eecd449138cf073a7ebc0df1499879dba
xavier.simmons <xavier.simmons@nyriad.com>
2017-03-14 19:10:48 +1300

    .* is evil - added only required imports

--- src/EncoderShort.java
+++ src/EncoderShort.java
@@ -1,19 +1,24 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.TreeMap;
+
 public class EncoderShort {
     private class Trie {
-        public long phrase_num = ++phrase_count; // Sets phrase_num and increments phrase count on construction
+        public final long phrase_num = ++phrase_count; // Sets phrase_num and increments phrase count on construction
         public TreeMap< Character, Trie > trie = new TreeMap< Character, Trie >();
     };
     private long phrase_count = 0; // 0 is RESET phrase number
     private Trie dict = new Trie(), dict_curr = dict;
     public static void main( String[] args ) throws IOException {
-    	BufferedReader br = new BufferedReader( new InputStreamReader( System.in, "UTF-8" ) );
-        BufferedWriter bw = new BufferedWriter( new OutputStreamWriter( System.out, "UTF-8" ) );
-        int dict_limit = 1 << Integer.parseInt( args[ 0 ] ); // 2 ^ max_num_bits
+    	final BufferedReader br = new BufferedReader( new InputStreamReader( System.in, "UTF-8" ) );
+        final BufferedWriter bw = new BufferedWriter( new OutputStreamWriter( System.out, "UTF-8" ) );
+        final int dict_limit = 1 << Integer.parseInt( args[ 0 ] ); // 2 ^ max_num_bits
         EncoderShort enc = new EncoderShort(); // Create encoder with empty dictionary
     	for( int next_c, c = br.read(); c != -1; c = next_c ) {
-			boolean last = ( ( next_c = br.read() ) == -1 ), in_dict = enc.dict_curr.trie.containsKey( (char)c );
+			final boolean last = ( ( next_c = br.read() ) == -1 ), in_dict = enc.dict_curr.trie.containsKey( (char)c );
 			if( enc.phrase_count == dict_limit )  { // Dictionary full
 				bw.write( Long.toString( ( enc.phrase_count = 0 ) /* RESET phrase # */ ) + "," + Integer.toHexString( c ) + "\n" );
 				enc = new EncoderShort(); // Reset dictionary

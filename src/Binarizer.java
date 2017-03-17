eec27fd06260483f3ddd18912ab2b0a786a3143a
xavier.simmons <xavier.simmons@nyriad.com>
2017-03-17 17:55:19 +1300

    removed imports from Encode & Decoder - Rewrote Binarizer & Debinarizer

--- src/Binarizer.java
+++ src/Binarizer.java
@@ -1,59 +1,50 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-public class Binarizer {
-    private static BufferedWriter bw;
+public class Binarizer
+{
     private long phrase_count = 0;
-    public final long reset_phrase_num = 0; // Ensured to work with any dictionary limit
     
-    private char parseMismatchByte( String token ) {
-		return (char)Integer.parseInt( token, 16 );
-	}
-    
-    private String convert( long value, int bits ) {
-    	String expression = "%" + Integer.toString( bits ) + "s";
-    	return String.format( expression, Long.toBinaryString( value ) ).replace(" ", "0");
+    private String toBinary( long value, int num_bits )
+    {
+        String expression = "%" + Integer.toString( num_bits ) + "s";
+        return String.format( expression, Long.toBinaryString( value ) ).replace( " ", "0" );
     }
 
-    public void binarize(String line) throws IOException {
-    	
-    	String[] tokens = line.split( ",", 2 ); // Limit to 2 for case where value is ','
-    	
-    	boolean has_mismatch = tokens.length == 2;
-    	long phrase_num = Long.parseLong( tokens[ 0 ] );
-    	
-    	if( phrase_count != 0 ){
-    		// Calculate minimum number of bits needed to represent number
-    		int bits = Long.SIZE - Long.numberOfLeadingZeros( phrase_count );
-        	bw.write( convert( phrase_num, bits ) );
-    	}
-    	
-    	phrase_count = phrase_num == reset_phrase_num ? 0 : phrase_count + 1;
-    	
-    	if( has_mismatch ) {
-    		char phrase_prefix = parseMismatchByte( tokens[ 1 ] );
-    		bw.write( convert( (long)phrase_prefix, 8 ) );
-    	}
-    }
-
-    public static void main(String[] args) throws IOException {
-
-    	BufferedReader br = new BufferedReader( new InputStreamReader( System.in, "UTF-8" ) );
-    	bw = new BufferedWriter( new OutputStreamWriter( System.out, "UTF-8" ) );
-
-        Binarizer binarizer = new Binarizer();
+    public void binarize( long phrase_num, char prefix, boolean has_mismatch )
+    {
+//         System.out.println( "> " + phrase_num + ", " + prefix + " | phrase_count: " + phrase_count );
+     
+        if( phrase_count != 0 ) // First symbol doesn't need phrase num encoded
+        {
+            // Calculate minimum number of bits needed to represent number
+            int num_bits = Long.SIZE - Long.numberOfLeadingZeros( phrase_count );
+            System.out.print( toBinary( phrase_num, num_bits ) );
+        }
         
-        try {
-            String line;
-    		while( ( line = br.readLine() ) != null ) {
-    			binarizer.binarize( line );
-    		}
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            br.close();
-            bw.close();
+        if( has_mismatch )
+        {
+            System.out.print( toBinary( prefix, 8 ) );
         }
 
+        phrase_count = phrase_num == 0 ? 0 : phrase_count + 1; // 0 is the RESET phrase number
+    }
+
+    public static void main( String[] args ) throws IOException
+    {
+        final BufferedReader br = new BufferedReader( new InputStreamReader( System.in, "UTF-8" ) );
+        Binarizer binarizer = new Binarizer();
+
+        for( String line = br.readLine(); line != null; line = br.readLine() )
+        {
+            String[] tokens = line.split( ",", 2 ); // Limit to 2 for case where value is ','
+            long phrase_num = Long.parseLong( tokens[ 0 ] );
+            char prefix = tokens.length == 2 ? (char)Integer.parseInt( tokens[ 1 ], 16 ) : '\0' /*can be anything*/;
+
+            binarizer.binarize( phrase_num, prefix, tokens.length == 2 );
+        }
+
+        br.close();
     }
 }
-
